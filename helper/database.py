9b284e3b19d2f61bafa087c83a003d27dbb88cdc
Venuja Thilakarathna <91406478+VenujaBots@users.noreply.github.com>
2021-11-12 17:25:13 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,8 +1,8 @@
 import pymongo 
 import os
 
-DB_NAME = os.environ.get("DB_NAME","VndGroup2")
-DB_URL = os.environ.get("DB_URL","mongodb+srv://VndGroup2:VndGroup2@cluster0.ge61u.mongodb.net/cluster0?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME","VndGroup3")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://VndGroup3:VndGroup3@cluster0.yw6va.mongodb.net/cluster0?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["USER"]
